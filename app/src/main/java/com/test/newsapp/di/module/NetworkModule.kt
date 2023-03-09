1a63a707aeeba7b8e0949b2a4c5116c5c8dc9d9b
NageshShenoy <nagesh.shenoy@robosoftin.com>
2023-03-09 11:18:19 +0530

    Removed the dagger dependency and respective files.

--- app/src/main/java/com/test/newsapp/di/module/NetworkModule.kt
+++ app/src/main/java/com/test/newsapp/di/module/NetworkModule.kt
@@ -1,38 +1,19 @@
 package com.test.newsapp.di.module
 
 import com.test.newsapp.data.remote.ApiNewsInterface
-import dagger.Module
-import dagger.Provides
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
 import retrofit2.converter.gson.GsonConverterFactory
-import javax.inject.Singleton
 
 
-class NetworkModule(){
+class NetworkModule {
 
-    val BASE_URL = "https://newsapi.org/v2/"
-
-    internal fun provideRetrofit(): Retrofit {
-
-        return Retrofit.Builder().baseUrl(BASE_URL)
-            .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
+    private val BASE_URL = "https://newsapi.org/v2/"
 
     internal fun provideRetrofitService(): ApiNewsInterface {
         return Retrofit.Builder().baseUrl(BASE_URL)
             .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
             .addConverterFactory(GsonConverterFactory.create())
             .build().create(ApiNewsInterface::class.java)
-    }
-
-    internal fun provideNetworkModule(): NetworkModule {
-        return this
-    }
-
-    internal fun provideBaseUrl(): String {
-        return BASE_URL
     }
 }
